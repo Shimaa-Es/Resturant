9f3d2c41553c7fa77dbcdf22d0c7aac8cafbd27f
Shimaa Mohamed <sheemy1999@gmail.com>
2025-03-09 20:39:15 +0200

    4rth

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,9 @@ import styles from "./Dashboard.module.css";
 export default function Dashboard() {
 
     return (
-        <div className="bg-danger h-100 flex-grow-1" id={styles.Dashboard}>
+        <div className=" h-100 flex-grow-1" id={styles.Dashboard}>
+            <h1>Dashboard</h1>
+            <p className="display-1 text-center text-danger fw-bolder bg-warning p-4 m-5"> أنا داش بورد صدقوني</p>
             
         </div>
     )
