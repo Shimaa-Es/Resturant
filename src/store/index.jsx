bc42d9e8ac4ae413916dc0565ca020feb147f5e2
Shimaa Mohamed <sheemy1999@gmail.com>
2025-03-09 20:28:03 +0200

    3erd

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,19 +1,20 @@
-
-// Global State categoriesData
 import coldImg from "../assets/imgs/categories/cold.png";
 import wokImg from "../assets/imgs/categories/wok.png";
 import dessertImg from "../assets/imgs/categories/desert.png";
+import pastaImg from "../assets/imgs/categories/pasta.png";
+import pizzaImg from "../assets/imgs/categories/pizza.png";
+import burgerImg from "../assets/imgs/categories/burger.png";
 import { create } from "zustand";
 // set Function can update any state into the object of global state
 export const useCategories = create((set) => (
     {
         data: [
             { documentId: 1, name: "Cold Drinks", path: "cold", imgUrl: coldImg },
-            { documentId: 2, name: "Burgers", path: "burgers", imgUrl: dessertImg },
-            { documentId: 3, name: "Pizza", path: "pizza", imgUrl: coldImg },
+            { documentId: 2, name: "Burgers", path: "burgers", imgUrl: burgerImg },
+            { documentId: 3, name: "Pizza", path: "pizza", imgUrl: pizzaImg },
             { documentId: 4, name: "Wok", path: "wok", imgUrl: wokImg },
             { documentId: 5, name: "Desserts", path: "dessert", imgUrl: dessertImg },
-            { documentId: 6, name: "Pasta", path: "pasta", imgUrl: wokImg },
+            { documentId: 6, name: "Pasta", path: "pasta", imgUrl: pastaImg},
         ],
 
         active_cat_id: 0,
